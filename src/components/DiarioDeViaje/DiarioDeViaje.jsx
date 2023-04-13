699a83b577d853756a6b662022a880cafc91ca3f
Martu Riesco <marturiesco.costa@hotmail.com>
2023-04-13 10:58:21 -0300

    agregando

--- src/components/DiarioDeViaje/DiarioDeViaje.jsx
+++ src/components/DiarioDeViaje/DiarioDeViaje.jsx
@@ -3,7 +3,29 @@ import './style.css'
 
 function DiarioDeViaje() {
   return (
-    <div>DiarioDeViaje</div>
+    <div>
+         <ul>
+<li className='items'>
+  <a href="#europa" className='link' >Europa</a>
+</li>
+<li className='items'>
+  <a href='#patagonia' className='link'  >Patagonia</a>
+</li>
+<li className='items'>
+  <a href='#estadosunidos' className='link' >Estados unidos</a>
+</li>
+<li className='items'>
+  <a href='#costaargentina' className='link' >Costa Argentina</a>
+</li>
+<li className='items'>
+  <a href='#mendoza' className='link' >Mendoza</a>
+</li>
+<li className='items'>
+  <a href='#sierras' className='link' >Sierras</a>
+  
+</li>
+</ul>
+    </div>
   )
 }
 
