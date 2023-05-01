d896259139e7e843f9449ebc2faa3f14b2268057
Martu Riesco <marturiesco.costa@hotmail.com>
2023-05-01 20:51:22 -0300

    haciendolo responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import './style.css'
+
 import { Link } from 'react-router-dom'
+import Menu from '../Navbar/Navbar'
 function Header() {
   return (
          <header>  
         <Link to='/'> <section className='loguito'><h2 className='titulo-pag'>A N A  </h2><h2 className='titulo-pag'>B A U E R </h2> </section></Link>
-          <nav>
-            <ul className='menu2'>
-       <Link to='/fotodigital'>  <li className="menu"> FOTO DIGITAL </li></Link>   
-       <Link to='/fotofija'>   <li className="menu">  FOTO 35mm  </li></Link>   
-       <Link to='/videos'>  <li className="menu">   VIDEOS  </li></Link>   
-       <Link to='/publicaciones'> <li className="menu">  REEL  </li></Link>   
-       <Link to='/aboutme'>  <li className="menu">  SOBRE MÍ</li></Link>   
-            </ul>
-        </nav>
+          <div className='menu2'>
+         {/*  <Navbar expand="lg">
+          <Container>
+         
+          <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+        <Nav.Link className="menu" href='/fotodigital'>  FOTO DIGITAL</Nav.Link>
+  <Nav.Link className="menu" href='/fotofija'>  FOTO 35mm</Nav.Link>  
+  <Nav.Link className="menu" href='/videos'>  VIDEOS </Nav.Link>
+            <Nav.Link className="menu" href='/publicaciones'>  REEL</Nav.Link>
+              <Nav.Link className="menu" href='/aboutme'>  SOBRE MÍ</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar> */}
+   <Menu/>
+
+         </div>
+      
         </header>
   )
 }
